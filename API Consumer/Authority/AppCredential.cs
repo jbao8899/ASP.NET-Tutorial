ce5fd7658c7e24df06eb9e9090fd7e86914e6b0e
jbao8899 <jbao8899@gmail.com>
2023-08-08 16:03:23 -0600

    Added controller for authentication

---
+++ API Consumer/Authority/AppCredential.cs
@@ -0,0 +1,8 @@
+﻿namespace API_Consumer.Authority
+{
+    public class AppCredential
+    {
+        public string ClientId { get; set; } = "";
+        public string Secret { get; set; } = "";
+    }
+}
---
+++ API Consumer/Controllers/AuthorityController.cs
@@ -0,0 +1,38 @@
+﻿using API_Consumer.Authority;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API_Consumer.Controllers
+{
+    [ApiController]
+    public class AuthorityController : ControllerBase
+    {
+        [HttpPost("auth")]
+        public IActionResult Authenticate([FromBody]AppCredential credential)
+        {
+            if (AppRepository.Authenticate(credential.ClientId, credential.Secret))
+            {
+                return Ok(new
+                {
+                    AccessToken = CreateToken(credential.ClientId),
+                    Expires = DateTime.UtcNow.AddMinutes(10)
+                });
+            }
+            else
+            {
+                ModelState.AddModelError("Unauthorized", "You are not authorized");
+                ValidationProblemDetails problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status401Unauthorized
+                };
+                return new UnauthorizedObjectResult(problemDetails);
+
+            }
+        }
+
+        private string CreateToken(string clientId)
+        {
+            return "";
+        }
+    }
+}
