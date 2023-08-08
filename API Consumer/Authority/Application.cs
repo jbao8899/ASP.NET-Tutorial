38df99853b8e7bb6fde2144ed33ff8bee5e70c68
jbao8899 <jbao8899@gmail.com>
2023-08-08 17:04:11 -0600

    Watched video 48, generated JWT token.

--- API Consumer/Authority/Application.cs
+++ API Consumer/Authority/Application.cs
@@ -3,10 +3,13 @@
     public class Application
     {
         public int ApplicationId { get; set; }
+
         public string? ApplicationName { get; set; }
 
         public string? ClientId { get; set; }
 
         public string? Secret { get; set;}
+
+        public string? Scopes { get; set; }
     }
 }
--- API Consumer/Controllers/AuthorityController.cs
+++ API Consumer/Controllers/AuthorityController.cs
@@ -1,21 +1,33 @@
 ﻿using API_Consumer.Authority;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace API_Consumer.Controllers
 {
     [ApiController]
     public class AuthorityController : ControllerBase
     {
+        IConfiguration _configuration;
+
+        public AuthorityController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpPost("auth")]
         public IActionResult Authenticate([FromBody]AppCredential credential)
         {
             if (AppRepository.Authenticate(credential.ClientId, credential.Secret))
             {
+                DateTime expires = DateTime.UtcNow.AddMinutes(10);
                 return Ok(new
                 {
-                    AccessToken = CreateToken(credential.ClientId),
-                    Expires = DateTime.UtcNow.AddMinutes(10)
+                    AccessToken = CreateToken(credential.ClientId, expires),
+                    Expires = expires
                 });
             }
             else
@@ -30,9 +42,32 @@ namespace API_Consumer.Controllers
             }
         }
 
-        private string CreateToken(string clientId)
+        private string CreateToken(string clientId, DateTime expiresAt)
         {
-            return "";
+            // Key
+            byte[]? secretKey = Encoding.ASCII.GetBytes(_configuration.GetValue<string>("SecretKey"));
+
+            Application? app = AppRepository.GetApplication(clientId);
+
+            // Claims
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim("AppName", app?.ApplicationName??string.Empty),
+                new Claim("Read", (app?.Scopes??string.Empty).Contains("read")?"true":"false"),
+                new Claim("Write", (app?.Scopes??string.Empty).Contains("write")?"true":"false")
+            };
+
+            JwtSecurityToken jwt = new JwtSecurityToken(
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(secretKey),
+                    SecurityAlgorithms.HmacSha256Signature
+                ),
+                claims: claims,
+                expires: expiresAt,
+                notBefore: DateTime.UtcNow
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
         }
     }
 }
